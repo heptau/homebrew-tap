35ab654e8591dc3bf16528a85251ee1cd1d39c3e
Zbyněk Vanžura <zbynek.vanzura@gmail.com>
2026-06-25 12:12:23 +0200

    pg_dbml v1.1.1

--- Formula/pg-dbml.rb
+++ Formula/pg-dbml.rb
@@ -1,9 +1,9 @@
 class PgDbml < Formula
   desc "Pure SQL exporter from PostgreSQL to DBML"
   homepage "https://github.com/heptau/pg_dbml"
-  url "https://github.com/heptau/pg_dbml/releases/download/v1.1.0/pg_dbml-1.1.0.tar.gz"
-  sha256 "6b220e40fd038c9f557897637f9ae6c05f4d4250fab9b0819068396a4976d8f1"
-  version "1.1.0"
+  url "https://github.com/heptau/pg_dbml/releases/download/v1.1.1/pg_dbml-1.1.1.tar.gz"
+  sha256 "4a9b60ed6c432649aa44e8cfe0975e39c541e991c9bcfd4c1720fa60a3ee8695"
+  version "1.1.1"
 
   depends_on "libpq"
 

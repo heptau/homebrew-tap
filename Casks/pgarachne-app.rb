501ccab5811d03f01011f679e297bde86d1ca258
Zbyněk Vanžura <zbynek.vanzura@gmail.com>
2026-07-01 23:41:19 +0200

    PgArachne v2.0.1

--- Casks/pgarachne-app.rb
+++ Casks/pgarachne-app.rb
@@ -1,17 +1,17 @@
 cask "pgarachne-app" do
-  version "2.0.0"
+  version "2.0.1"
   name "PgArachne"
   desc "GUI wrapper for PgArachne"
   homepage "https://www.pgarachne.com/"
 
   on_arm do
     url "https://github.com/heptau/pgarachne/releases/download/v#{version}/pgarachne-macos-arm64-app.zip"
-    sha256 "537e5ed2b6b378a87d060a551bc41e897092419a19af710ddd05388ffc83ca03"
+    sha256 "90fdc549feaed3eaaf769caac8535a9c3a95499ddfce176ee1b04368190af2a4"
   end
 
   on_intel do
     url "https://github.com/heptau/pgarachne/releases/download/v#{version}/pgarachne-macos-amd64-app.zip"
-    sha256 "1d72c6994129a55ae2ebf060ac466b286b09b740f3970ad7b521cddea704025b"
+    sha256 "329d3239ab3307c08fa4dde020d0f652d00cb1a8e5becdd919558ade26bd3b38"
   end
 
   app "PgArachne.app"
